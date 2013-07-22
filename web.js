f374e060a6e1ef35d0eae5b0f2b29b6a9737a7e2
Annie Ngai <i.am.annie@live.ca>
2013-07-22 05:10:10 +0000

    print index.html added

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var index = fs.readFileSync('index');
+  var indexstring = index.toString();
+  response.send(indexstring);
 });
 
 var port = process.env.PORT || 5000;
